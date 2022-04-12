f2ce2983d868738e0429452b33c7733a7ebcb497
Oswald Quevillart <o.quevillart@gmail.com>
2022-04-12 12:34:18 +0200

    ajouts entités pour connexion et demande de contacts des utilisateurs

---
+++ src/Entity/UserContact.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserContactRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserContactRepository::class)]
+class UserContact
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $user_lastname;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $user_firstname;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $user_mail;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $object;
+
+    #[ORM\Column(type: 'text')]
+    private $message;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private $send_at;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserLastname(): ?string
+    {
+        return $this->user_lastname;
+    }
+
+    public function setUserLastname(string $user_lastname): self
+    {
+        $this->user_lastname = $user_lastname;
+
+        return $this;
+    }
+
+    public function getUserFirstname(): ?string
+    {
+        return $this->user_firstname;
+    }
+
+    public function setUserFirstname(string $user_firstname): self
+    {
+        $this->user_firstname = $user_firstname;
+
+        return $this;
+    }
+
+    public function getUserMail(): ?string
+    {
+        return $this->user_mail;
+    }
+
+    public function setUserMail(string $user_mail): self
+    {
+        $this->user_mail = $user_mail;
+
+        return $this;
+    }
+
+    public function getObject(): ?string
+    {
+        return $this->object;
+    }
+
+    public function setObject(string $object): self
+    {
+        $this->object = $object;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getSendAt(): ?\DateTimeImmutable
+    {
+        return $this->send_at;
+    }
+
+    public function setSendAt(\DateTimeImmutable $send_at): self
+    {
+        $this->send_at = $send_at;
+
+        return $this;
+    }
+}
